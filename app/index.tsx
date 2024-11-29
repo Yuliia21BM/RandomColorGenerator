84a8a258f2e470f2f0a9b0a43110431497dafbee
YuliiaM-UT <108929319+Yuliia21BM@users.noreply.github.com>
2024-11-29 23:40:18 +0200

    feat: add fonts, splash screen and app icon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+/*
+ *     FRAMEWORK
+ */
+import React, { useEffect } from "react";
+/*
+ *     COMPONENTS
+ */
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { HomeColoredContent } from "./components/HomeColoredContent";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { APP_COLORS } from "@/utils/colors";
 
-export default function Index() {
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [loaded, error] = useFonts({
+    "EduAUVICWANTArrows-Regular": require("../assets/fonts/EduAUVICWANTArrows-Regular.ttf"),
+    "EduAUVICWANTArrows-Bold": require("../assets/fonts/EduAUVICWANTArrows-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor={APP_COLORS.background} />
+      <HomeColoredContent />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: APP_COLORS.background,
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+});
