5d8fe533c0d43e1c7484c9b5f3196df7efe90fbf
YuliiaM-UT <108929319+Yuliia21BM@users.noreply.github.com>
2024-11-29 23:40:45 +0200

    feat: add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
+/*
+ *     FRAMEWORK
+ */
+import React from "react";
+/*
+ *     NAVIGATION
+ */
 import { Stack } from "expo-router";
+/*
+ *     HOOKS
+ */
+import { CurrentBgColorProvider } from "@/hooks/useCurrentBgColor";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <CurrentBgColorProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </CurrentBgColorProvider>
+  );
 }
